21078410b81ce5fb4bcc997dacf43d59f03ae273
aaa0742 <137038424+aaa0742@users.noreply.github.com>
2023-08-18 13:24:18 +0900

    엑셀 다운로드 관련 리팩토링

--- src/main/java/kr/co/seedit/domain/mapper/seedit/ReportDao.java
+++ src/main/java/kr/co/seedit/domain/mapper/seedit/ReportDao.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import kr.co.seedit.domain.hr.dto.BasicSalaryDto;
 import kr.co.seedit.domain.hr.dto.MonthlyKeunTaeDto;
 import kr.co.seedit.domain.hr.dto.ReportMonthlyKeunTaeDto;
+import kr.co.seedit.domain.hr.dto.ReportParamsDto;
 import kr.co.seedit.domain.hr.dto.ReportPayrollDto;
 import kr.co.seedit.domain.hr.dto.SalaryExcelDto;
 
@@ -17,9 +18,11 @@ import kr.co.seedit.domain.hr.dto.SalaryExcelDto;
 @Mapper
 public interface ReportDao {
 	
-	List<SalaryExcelDto> findSalaryExcel(BasicSalaryDto basicSalaryDto);
+	List<SalaryExcelDto> findERPIUData(ReportParamsDto reportParamsDto);
+	
+	List<ReportPayrollDto> findPayrollData(ReportParamsDto reportParamsDto);
 	
 //	List<ReportMonthlyKeunTaeDto> findPayroll(MonthlyKeunTaeDto monthlyKeunTaeDto);
 	
-	List<ReportPayrollDto> findPayrollReport(ReportPayrollDto reportPayrollDto);
+	
 }
