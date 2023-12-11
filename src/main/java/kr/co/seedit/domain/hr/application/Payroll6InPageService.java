51494ddfa37830ed58c4d89c014677a3bb275cd3
lhkyu <51306771+lhkyu@users.noreply.github.com>
2023-12-11 13:39:24 +0900

    연봉제 연차수당 위치 변경, 무급 명칭 추가 (급여표, 개인급여내역)

--- src/main/java/kr/co/seedit/domain/hr/application/Payroll6InPageService.java
+++ src/main/java/kr/co/seedit/domain/hr/application/Payroll6InPageService.java
@@ -337,6 +337,10 @@ public class Payroll6InPageService {
         if (null != data.getAttribute01() && data.getEmployeeType().equals("200")) {
             sheet.getRow(curRow + 12).getCell(curCol + 3).setCellValue(data.getAttribute01());
         }
+        //연봉제 연차수당
+        if (0 != data.getAttribute01() && data.getEmployeeType().equals("100")) {
+            sheet.getRow(curRow + 12).getCell(curCol + 3).setCellValue(data.getAttribute01());
+        }
 
         //연차정산
         if (null != data.getAnnualLeaveCalc()) {
@@ -401,9 +405,9 @@ public class Payroll6InPageService {
         }
         //연봉제 무급처리 금액
         if (0 != data.getAttribute01() && data.getEmployeeType().equals("100")) {
+        	sheet.getRow(curRow + 19).getCell(curCol).setCellValue("무급");
             sheet.getRow(curRow + 19).getCell(curCol + 1).setCellValue(data.getNonPayDay());
             sheet.getRow(curRow + 19).getCell(curCol + 2).setCellValue(data.getNonPayCnt());
-            sheet.getRow(curRow + 19).getCell(curCol + 3).setCellValue(data.getAttribute01());
         }
         //보조금
         //교통비
